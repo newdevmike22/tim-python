47dcd4a2919ddb585ca4fab020565d47045d3b7d
MLDODSON <paola@dominicansoul.com>
2023-12-09 13:16:17 -0600

    First Python Project
    First Python Project. Learning to use the print function, strings and numbers.

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,13 @@
+print('Hello, World!')
+print("Buenos Dias!")
+print(10 + 6)
+print(8 * 7)
+print(9 / 3)
+print()
+print('The end friend.', 'Or is it?', 'Much more python to learn.', 55)
+
+# Coding Challange - Print out the string (Make my funk the P-Funk!)
+print("Make my funk the P-Funk!")
+
+# Coding Challange - Printing the result of this calculation (76 * 2)
+print(76 * 2)
