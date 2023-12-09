77905f856d6cc5a71e71bbbb248e3318f18bd0d6
MLDODSON <paola@dominicansoul.com>
2023-12-09 17:34:33 -0600

    Using escape characters in Python
    Using escape characters in Python.

---
+++ EscapeCharacters/escapeChar.py
@@ -0,0 +1,25 @@
+# Splitting a string
+splitString = "This string has been\nsplit over\nseveral \nlines."
+print(splitString)
+
+# Tabbing a string
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+# or
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
+
+anotherSplitString = """Make my funk
+the P-funk.
+I want to get
+funked up!"""
+
+print(anotherSplitString)
+
+print("Number 1\tThe Larch")
+print("Number 2\tThe Horse Chestnut")
+
+# Escaping an escape character
+print("C:\\Users\\mikedodson\\notes.txt")
