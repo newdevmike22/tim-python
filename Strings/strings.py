d316962fe40ce4ea320a278e135ddb8ca5e96f3b
MLDODSON <paola@dominicansoul.com>
2023-12-09 14:08:14 -0600

    Learning how to use Python strings.
    Learning how to use Python strings and concatenation.

---
+++ Strings/strings.py
@@ -0,0 +1,14 @@
+print("Today is a great day to learn Python!")
+print('Python is fun to learn.')
+print("Python's strings are easy to use.")
+print('We can even include "quotes" in strings')
+
+#String Concatenation
+print("Hello Betty." + " Is it still raining outside?")
+
+greeting = "What's cooking"
+person = "Joe"
+greetingTwo = "Bonjour"
+name = input("Please enter your name ")
+print(greeting + " " + person + "?")
+print(greetingTwo + " " + name)
